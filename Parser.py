ce4f1fe9cb03929b7c7f84588c7ff545aab73777
Reid Whitaker <reidmwhitaker@gmail.com>
2017-06-26 18:54:48 -0400

    Updates to CL and CAP parsing

--- Parser.py
+++ Parser.py
@@ -3,22 +3,23 @@ import os
 from bs4 import BeautifulSoup
 import json
 import re
+import warnings
 
+#determines how a document should be parsed and send the data to the appropriate parser
+#Currently supports .xml files from the Library Innovation Lab's Caselaw Access Project
+#and .json files from CourtListener's API
 def parse(data):
-    if "HLS-CASELAW-CASEXML" in data:
+    if 'HLS-CASELAW-CASEXML' in data:
         parse_CAP(data)
-    if '"resource_uri": http://www.courtlistener.com/api/rest/v3/opinions/' in data:
+    if "http://www.courtlistener.com/api/rest/v3" in data:
         parse_CL(data)
 
-#todo: Note footnote numbering may not be seperated from the first word of the footnote. May cause issues if footnote is solely a citation
-#todo: recusials
+#todo: SHould be ok -- Note footnote numbering may not be seperated from the first word of the footnote. May cause issues if footnote is solely a citation
+#todo: recusals!
 #todo: joinings & seperate opinions ... looks for need to see how consistently it works
+#todo: check percuriam authoriships
 def parse_CAP(data):
-    data = re.sub(r'(\D)(\u00ad)(\D)', r'\1\3',data)
-    data = re.sub(r'(\D)(\-)(\D)', r'\1\3',data)
-
-    soup = BeautifulSoup(data, "xml")
-
+    #todo: Currently unused
     def try_add_2_json(json_2_b,name,tag_to_try):
         try:
             tag_to_try
@@ -28,6 +29,12 @@ def parse_CAP(data):
             json_2_b[name]=tag_to_try
         return json_2_b
 
+    data = re.sub(r'(\D)(\u00ad)(\D)', r'\1\3',data)
+    data = re.sub(r'(\D)(\-)(\D)', r'\1\3',data)
+    data = re.sub(r'\u2019',"'",data)
+    data = re.sub(r'\u201c', '"', data)
+    data = re.sub(r'\u201d', '"', data)
+    soup = BeautifulSoup(data, "xml")
     json_2_b={}
     case_id = soup.case['caseid']
     json_2_b['id'] = case_id
@@ -53,38 +60,59 @@ def parse_CAP(data):
     case_name_short = name_tag['abbreviation']
     json_2_b['case_name_short'] = case_name_short
     count_attorneys = 1
+    #Collect attorney information
     for tag in soup.find_all('attorneys'):
         json_2_b['attorney_'+str(count_attorneys)] = tag.string
         count_attorneys = count_attorneys + 1
+    #Collect opinion text and authors for each opinion
     count_opinions = 1
     for tag in soup.find_all('opinion'):
-        json_2_b['opinion_type_' + str(count_opinions)] = tag['type']
-        try:
-            tag.author.string
-        except:
-            pass
-        else:
-            json_2_b['opinion_' + str(count_opinions)+ "_author"] = tag.author.string
         json_2_b['opinion_text_' + str(count_opinions)] = ""
         for p_tag in tag.find_all('p'):
             if p_tag.string is not None:
-                json_2_b['opinion_text_' + str(count_opinions)] = json_2_b['opinion_text_' + str(count_opinions)] + " " + p_tag.string
+                json_2_b['opinion_text_' + str(count_opinions)] = json_2_b['opinion_text_' + str(
+                    count_opinions)] + " " + p_tag.string
             else:
                 for string in p_tag.stripped_strings:
-                    json_2_b['opinion_text_' + str(count_opinions)] = json_2_b['opinion_text_' + str(count_opinions)] + " " + string
+                    json_2_b['opinion_text_' + str(count_opinions)] = json_2_b['opinion_text_' + str(
+                        count_opinions)] + " " + string
+        json_2_b['opinion_type_' + str(count_opinions)] = tag['type']
+        #Get opinion author information
+        try:
+            tag.author.string
+        except:
+            if re.match(r'(?:\s*Opinion\sof\sthe\sCourt.)|(?:\s*Order\sof\sthe\sCourt)',
+                        json_2_b['opinion_text_' + str(count_opinions)], re.IGNORECASE):
+                json_2_b['opinion_' + str(count_opinions)+ "_author"] = "per curium"
+            try:
+                json_2_b['opinion_' + str(count_opinions) + "_author"]
+            except:
+                pass
+            else:
+                if json_2_b['opinion_' + str(count_opinions)+ "_author"] is None:
+                    json_2_b['opinion_' + str(count_opinions)+ "_author"] = "per curium"
+        else:
+            json_2_b['opinion_' + str(count_opinions)+ "_author"] = tag.author.string
         count_opinions = count_opinions + 1
-
     m = 1
+    #Finding joining justices
     while m < count_opinions:
-        matches = re.findall(r'JUSTICE\s(\w+)\sjoins|(?:Chief\sJustice\s(\w+?)\sand\s)?(?:Justices\s(\w+)\,\s)?(?:(\w+)\,\s)?(?:(\w+)\,\s)?(?:(\w+)\,\s)?(?:(\w+)\,?\sand\s)?(\w+?)\sconcurred',json_2_b['opinion_text_' + str(m)],re.IGNORECASE)
+        join_count = 1
+        matches = re.findall(r'JUSTICE\s(\w+)\sjoins|(?:Chief\sJustice\s(\w+?)\sand\s)?(?:Justices\s(\w+)\,\s)?'
+                             r'(?:(\w+)\,\s)?(?:(\w+)\,\s)?(?:(\w+)\,\s)?(?:(\w+)\,?\sand\s)?(\w+?)\sconcurred',
+                             json_2_b['opinion_text_' + str(m)],re.IGNORECASE)
         if matches:
-            join_count = 1
             for match in matches:
                 for group in match:
                     if group != "" and group != "specially":
                         json_2_b['opinion_' + str(m) + '_joining_' + str(join_count)] = group
                         #print(json_2_b['opinion_' + str(m) + '_joining_' + str(join_count)])
                         join_count = join_count + 1
+        #matches_2 = re.findall(r'JUSTICE\s([a-zA-Z]+)\sjoins\sin\sthis',json_2_b['opinion_text_' + str(m)],re.IGNORECASE)
+        #if matches_2:
+        #    for match in matches_2:
+        #        json_2_b['opinion_' + str(m) + '_joining_' + str(join_count)] = match
+        #        join_count + 1
         m = m+1
 
     json_out = json.dumps(json_2_b, indent=4, separators=(',', ': '))
@@ -95,31 +123,29 @@ def parse_CAP(data):
 
 def parse_CL(json_CL):
     #todo: fill out all available and relevant metadata
-    warnings.filterwarnings('ignore',message=r".*looks like a URL. Beautiful Soup is not an HTTP client. You should probably.*")
+    #print("checkpoint")
+    warnings.filterwarnings('ignore',
+                            message=r".*looks like a URL. Beautiful Soup is not an HTTP client. You should probably.*")
     data = json.loads(json_CL)
     cluster = BeautifulSoup(data['cluster'], 'html.parser').text
     id = re.match(r"http://www.courtlistener.com/api/rest/v3/clusters/(\d+)", cluster)
-    cluster = OpinionCluster(id=id.group(1))
-    opinion = Opinion()
+    case_id=id.group(1)
 
     absolute = BeautifulSoup(data['absolute_url'], 'html.parser').text
     name = re.match(r"/opinion/(\d+)/(.*)/", absolute)
-    opinion.name = name.group(1) + "_" + name.group(2)
+    name = name.group(1) + "_" + name.group(2)
 
-    opinion.text = BeautifulSoup(data['html'], 'html.parser')
-    cluster.opinion = opinion
     if data['html'] != "":
-        opinion.text = BeautifulSoup(data['html'],'html.parser')
+        text = BeautifulSoup(data['html'],'html.parser')
     #todo: fix parsing of decisions without html entries
     else:
-        opinion.text = BeautifulSoup(data['html_with_citations'], 'html.parser')
+        text = BeautifulSoup(data['html_with_citations'], 'html.parser')
 
     cites = []
-    for tag in opinion.text.find_all('p', class_='case_cite'):
+    for tag in text.find_all('p', class_='case_cite'):
         cites.append(tag.text)
-    opinion.cite = cites
 
-    reduced_soup = opinion.text.find_all('div', class_="num")
+    reduced_soup = text.find_all('div', class_="num")
     opinion_text = ""
     for tag in reduced_soup:
         opinion_text_soup = tag.find_all('p', class_="indent")
@@ -127,20 +153,24 @@ def parse_CL(json_CL):
             opinion_text = opinion_text + tag.prettify()
 
     # todo: fix footnote parsing...make sure linked footnotes are present
-    footnotes = opinion.text.find_all('div', class_="footnote", id=re.compile("^fn\d+"))
+    footnotes = text.find_all('div', class_="footnote", id=re.compile("^fn\d+"))
     for tag in footnotes:
         opinion_text = opinion_text + tag.prettify()
-    opinion.opinion_text = opinion_text
 
-    if opinion.opinion_text == "":
-        warnings.warn(opinion.name + " failed to parse")
-    else:
-        os.makedirs("./parsed_cases/", exist_ok=True)
-        doc = open("./parsed_cases/" + opinion.name + "_parsed", 'w')
-        doc.write(opinion.opinion_text)
-        doc.close()
+    json_2_b = {}
+    json_2_b['opinion_text_1']=opinion_text
+    json_2_b['cite']=cites
+    json_2_b['name']=name
+    json_2_b['id']=case_id
+    json_out = json.dumps(json_2_b, indent=4, separators=(',', ': '))
+    os.makedirs("./parsed_cases", exist_ok=True)
+    doc = open("./parsed_cases/" + case_id + "_" + name + ".json", 'w')
+    doc.write(json_out)
+    doc.close()
+    #print(json_out)
 
-def get_dir(dir="./../Illinois_CAP_Sample"):
+#Get the directory where the cases to be parsed are stored
+def get_dir(dir="./../Parser_Cases"):
     return dir
 
 #todo: develop algorithm to split opinion clusters
@@ -179,6 +209,7 @@ def daubert_Test():
         cites.append(tag.text)
     daubert.cite = cites
 
+#Loads files and sends each file to the parser
 def main():
     #print("start")
     dir = get_dir()
@@ -186,7 +217,7 @@ def main():
         for file in files:
             #For testing purposes
             #print(os.path.join(root,file))
-            if not file.startswith("."):
+            if file.endswith(".xml") or file.endswith(".json"):
                 data = open(os.path.join(root,file)).read()
                 parse(data)
     #print("end")
